a852966084e4d5dcf8447303a306c5472af3205c
App Academy Student <student@appacademy.io>
2016-11-18 14:07:17 -0800

    Add build move tree and path finder methods

--- kpf.rb
+++ kpf.rb
@@ -1,4 +1,4 @@
-require_relative "tree_node"
+require_relative "00_tree_node"
 
 class KnightPathFinder
   KNIGHT_MOVES = [[-2, 1], [-2, -1], [2, 1], [2, -1], [1, -2], [1, 2], [-1, -2], [-1, 2]]
@@ -7,13 +7,13 @@ class KnightPathFinder
 
   def initialize(pos)
     @position = pos
-    @grid = Array.new(8) { Array.new(8) }
     @visited_positions = [pos]
+    build_move_tree
   end
 
   def new_move_positions(pos)
-    candidates = self.valid_moves(pos)
-    candidates.reject! {|candidate| @visited_positions.include?(candidate)}
+    candidates = self.class.valid_moves(pos)
+    candidates.reject! { |candidate| @visited_positions.include?(candidate)}
     @visited_positions += candidates
     candidates
   end
@@ -33,5 +33,38 @@ class KnightPathFinder
   def self.move_in_bounds?(pos)
     pos.none? { |num| num < 0 || num >= 8 }
   end
+
+  def build_move_tree
+    @root_node = PolyTreeNode.new(position)
+
+    queue = [@root_node]
+
+    until queue.empty?
+      current_node = queue.shift
+      next_positions = new_move_positions(current_node.value)
+      next_positions.each do |pos|
+        next_node = PolyTreeNode.new(pos)
+        current_node.add_child(next_node)
+        queue << next_node
+      end
+    end
+  end
+
+  def trace_path_back(end_node)
+    path = []
+    current_node = end_node
+    until path.last == @position
+      path << current_node.value
+      current_node = current_node.parent
+    end
+    path
+  end
+
+  def find_path(end_pos)
+    end_node = @root_node.dfs(end_pos)
+    trace_path_back(end_node).reverse
+  end
+
+
 
 end
